3952829936a97596ce1c5879442b75593063a614
Dmitry Khomyakov <hds2711@mail.ru>
2024-10-22 21:52:25 +0800

    feat: Реализовать удаление из таблицы

--- table/index.php
+++ table/index.php
@@ -8,15 +8,19 @@ session_start();
 
 if (isset($_GET['value'])) {
     $_SESSION['selected_value'] = $_GET['value'];  // Экранирование
-    $selected_value = $_SESSION['selected_value'];
+}
+$selected_value = $_SESSION['selected_value'];
+if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $res = $connection->query("DELETE FROM `$selected_value` WHERE `ID`=$id");
 }?>
     <table>
             <thead>
             <tr>
-
     <?$results = $connection->query("SELECT * FROM $selected_value");
     if ($results) {
         while ($row = $results->Fetch()) {
+
             foreach ($row as $key => $value) {?>
                 <th><?=$key?></th>
           <?  }?>
@@ -28,12 +32,19 @@ if (isset($_GET['value'])) {
                 <td><?=$value?></td>
             <?  }?>
             <td>
-                <a href="#">Изменить</a> | <a href="#">Удалить</a>  <!-- Добавили ссылки на изменение и удаление -->
+                <a href="#">Изменить</a> | <a href="#" onclick="confirmDelete(<?=$row['ID']?>)">Удалить</a>
             </td>
              </tr>
             </tbody>
        <? }
     }?>
-
     </table>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
+    <script>
+        function confirmDelete(id) {
+            if (confirm('Вы уверены, что хотите удалить эту запись?')) {
+                window.location.href = '?action=delete&id=' + id;
+            }
+        }
+    </script>
+<? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
