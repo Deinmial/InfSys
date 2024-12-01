1961800408ee60f42ec7ad3e3fa69a981d97401b
Dmitry Khomyakov <hds2711@mail.ru>
2024-12-01 19:18:18 +0800

    feat: Добавить заготовку формы

--- change/index.php
+++ change/index.php
@@ -8,17 +8,27 @@ session_start();
 
 if (isset($_GET['row'])) {
     $_SESSION['selected_row'] = $_GET['row'];  // Экранирование
-}
+}?>
+    <form action="update.php" method="post">
+<?
 $selected_row = $_SESSION['selected_row'];
 $results = $connection->query("SELECT * FROM $selected_row");
 if ($results) {
     while ($row = $results->Fetch()) {
-        echo '<pre>';
-        print_r($row);
-        echo '</pre>';
+        foreach ($row as $key => $value){
+            if($key == 'ID') {
+                continue;
+            }?>
+            <label><?=$key?></label>
+            <input value="<?=$value?>">
+            <br>
+            <br>
+<?
+        }
     }
 }
-
 ?>
+        <input type="submit" value="Обновить">
+    </form>
 
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- create/index.php
+++ create/index.php
@@ -8,17 +8,47 @@ session_start();
 
 if (isset($_GET['table'])) {
     $_SESSION['selected_table'] = $_GET['table'];  // Экранирование
+}?>
+    <form method="post">
+        <?
+if (isset($_POST['submit_button'])) {
+    $selected_table = $_SESSION['selected_table'];
+    $fields = [];
+    $values = [];
+    foreach ($_POST as $key => $value) {
+        if (!empty($value) && $key != 'submit_button') { // Проверка, что поле не равно 'submit_button'
+            $fields[] = $key;
+            $values[] = $value;
+        }
+    }
+
+    $selected_table = htmlspecialchars($selected_table);
+
+    //LocalRedirect('/table/?value='.$selected_table);
+    if (!empty($fields) && !empty($values)) {
+        $connection->query("INSERT INTO $selected_table (" . join(', ', $fields) . ") VALUES (" . join(', ', $values) . ")");
+    }
 }
+?>
+<?
 $selected_table = $_SESSION['selected_table'];
 $results = $connection->query("SELECT * FROM $selected_table");
 if ($results) {
     while ($row = $results->Fetch()) {
-        echo '<pre>';
-        print_r($row);
-        echo '</pre>';
+        foreach ($row as $key => $value){
+            if($key == 'ID') {
+                continue;
+            }?>
+            <label><?=$key?></label>
+            <input name="<?=$key?>">
+            <br>
+            <br>
+            <?
+        }
     }
 }
-
 ?>
+    <input type="submit" name="submit_button" value="Добавить">
+    </form>
 
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
