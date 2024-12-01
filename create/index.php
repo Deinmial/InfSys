151b5685a9b8a407c5b9090096afb6e5d94c899f
Dmitry Khomyakov <hds2711@mail.ru>
2024-12-02 00:31:00 +0800

    feat: Реализовать добавления данных в БД

--- create/index.php
+++ create/index.php
@@ -8,47 +8,50 @@ session_start();
 
 if (isset($_GET['table'])) {
     $_SESSION['selected_table'] = $_GET['table'];  // Экранирование
-}?>
-    <form method="post">
-        <?
-if (isset($_POST['submit_button'])) {
-    $selected_table = $_SESSION['selected_table'];
+}
+$selected_table = $_SESSION['selected_table'];
+
+// Обработка формы, если она отправлена
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $fields = [];
     $values = [];
+
+    // Собираем данные из формы
     foreach ($_POST as $key => $value) {
-        if (!empty($value) && $key != 'submit_button') { // Проверка, что поле не равно 'submit_button'
-            $fields[] = $key;
-            $values[] = $value;
-        }
+        $fields[] = $key;
+        $values[] = "'" . $connection->getSqlHelper()->forSql($value) . "'";
     }
 
-    $selected_table = htmlspecialchars($selected_table);
+    // Формируем SQL-запрос
+    $sql = "INSERT INTO $selected_table (" . implode(', ', $fields) . ") VALUES (" . implode(', ', $values) . ")";
 
-    //LocalRedirect('/table/?value='.$selected_table);
-    if (!empty($fields) && !empty($values)) {
-        $connection->query("INSERT INTO $selected_table (" . join(', ', $fields) . ") VALUES (" . join(', ', $values) . ")");
-    }
+    // Выполняем запрос
+    $connection->queryExecute($sql);
+
+    LocalRedirect('/table/?value='.$selected_table);
 }
 ?>
-<?
-$selected_table = $_SESSION['selected_table'];
-$results = $connection->query("SELECT * FROM $selected_table");
-if ($results) {
-    while ($row = $results->Fetch()) {
-        foreach ($row as $key => $value){
-            if($key == 'ID') {
-                continue;
-            }?>
-            <label><?=$key?></label>
-            <input name="<?=$key?>">
-            <br>
-            <br>
-            <?
+
+    <!-- Форма для добавления данных -->
+    <form method="POST" onsubmit="return confirm('Вы уверены, что хотите добавить данные?');">
+        <?
+        // Вывод данных из таблицы
+        $results = $connection->query("SELECT * FROM $selected_table LIMIT 1");
+        if ($results) {
+            while ($row = $results->Fetch()) {
+                foreach ($row as $key => $value) {
+                    if($key == 'ID') {
+                        continue;
+                    }?>
+                    <label for="<?=$key?>"><?=$key?>:</label>
+                    <input type="text" name="<?=$key?>" id="<?=$key?>"><br><br>
+                <?}
+            }
         }
-    }
-}
-?>
-    <input type="submit" name="submit_button" value="Добавить">
+
+        ?>
+
+        <button type="submit">Добавить данные</button>
     </form>
 
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
