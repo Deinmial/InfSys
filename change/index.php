1b9f3019e55373d21058963b9dc1f942161f8cd1
Dmitry Khomyakov <hds2711@mail.ru>
2024-12-02 17:07:52 +0800

    feat: Добавить обновление данных в записи БД

--- change/index.php
+++ change/index.php
@@ -6,29 +6,53 @@ $connection = Bitrix\Main\Application::getConnection();
 
 session_start();
 
+if (isset($_GET['table'])) {
+    $_SESSION['selected_table'] = $_GET['table'];  // Экранирование
+}
 if (isset($_GET['row'])) {
     $_SESSION['selected_row'] = $_GET['row'];  // Экранирование
-}?>
-    <form action="update.php" method="post">
-<?
+}
+
+$selected_table = $_SESSION['selected_table'];
 $selected_row = $_SESSION['selected_row'];
-$results = $connection->query("SELECT * FROM $selected_row");
-if ($results) {
-    while ($row = $results->Fetch()) {
-        foreach ($row as $key => $value){
-            if($key == 'ID') {
-                continue;
-            }?>
-            <label><?=$key?></label>
-            <input value="<?=$value?>">
-            <br>
-            <br>
-<?
-        }
+
+// Обработка формы, если она отправлена
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $updates = [];
+
+    // Собираем данные из формы
+    foreach ($_POST as $key => $value) {
+        $updates[] = "$key = '" . $connection->getSqlHelper()->forSql($value) . "'";
     }
+
+    // Формируем SQL-запрос
+    $sql = "UPDATE $selected_table SET " . implode(', ', $updates) . " WHERE ID = $selected_row";
+
+    // Выполняем запрос
+    $connection->queryExecute($sql);
+
+    LocalRedirect('/table/?value='.$selected_table);
 }
 ?>
-        <input type="submit" value="Обновить">
+
+    <!-- Форма для редактирования данных -->
+    <form method="post" onsubmit="return confirm('Вы уверены, что хотите изменить данные?');">
+        <?
+        $results = $connection->query("SELECT * FROM $selected_table WHERE ID=$selected_row");
+        if ($results) {
+            while ($row = $results->Fetch()) {
+                foreach ($row as $key => $value){
+                    if($key == 'ID') {
+                        continue;
+                    }?>
+                    <label for="<?=$key?>"><?=$key?>:</label>
+                    <input type="text" name="<?=$key?>" id="<?=$key?>" value="<?=$value?>"><br><br>
+                    <?
+                }
+            }
+        }
+        ?>
+        <button type="submit">Изменить данные</button>
     </form>
 
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
